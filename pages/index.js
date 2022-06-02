0f2445517faad65638cd40c7b7e3289379d29db3
Pankaj Phartiyal <pankajphartiyal90@gmail.com>
2022-06-02 07:18:43 +0530

    getting data from contentful

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,25 @@
-import Link from 'next/link'
+export default function IndexPage(props) {
+  console.log(props);
+  return <div>Hello World. </div>;
+}
 
-export default function IndexPage() {
-  return (
-    <div>
-      Hello World.{' '}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </div>
-  )
+export async function getServerSideProps(context) {
+  const response = await fetch(
+    "https://graphql.contentful.com/content/v1/spaces/wn1reipdztrw/",
+    {
+      headers: {
+        authorization: "Bearer 6Eb9ZGQrMcRFqgmeSj1I6z_aPzuQR30RBvs4FPQEXXI",
+        "content-type": "application/json"
+      },
+      body:
+        '{"query":"query {\\n  pageCollection(limit: 1) {\\n    items {\\n      title\\n      slug\\n    }\\n  }\\n}","variables":null}',
+      method: "POST"
+    }
+  );
+  const data = await response.json();
+  return {
+    props: {
+      data
+    }
+  };
 }
